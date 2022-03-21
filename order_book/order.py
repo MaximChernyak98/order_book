caa1ddc1ae129042bbc15fc27c4ef6f7e1ecc93d
Cherniak_Maksim <cherniak_m@utkonos.ru>
2022-03-21 22:13:50 +0300

    add get info function

--- order_book/order.py
+++ order_book/order.py
@@ -7,3 +7,6 @@ class Order:
     order_type: str = 'bid'
     price: int = 0
     volume: int = 0
+
+    def __repr__(self):
+        return f'Order with id={self.order_id}, type={self.order_type}, price={self.price}, volume={self.volume}'
